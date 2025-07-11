905c044ccd7dcfe3b5c181212697f4861f1639b8
HendySetiawan <hendy.sevenfoldism@gmail.com>
2025-07-12 01:49:12 +0700

    Fix: Menghapus reportWebVitals import dari index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // Pastikan ini ada dan mengimpor App
-import reportWebVitals from './reportWebVitals'; // Opsional, bisa dihapus kalau mau
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,8 +8,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(); // Opsional, bisa dihapus
